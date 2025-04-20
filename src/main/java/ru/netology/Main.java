bd4858add95d1fc7510da1587a6fcc23ae33cfd4
Sinsl <sinsl@me.com>
2025-04-20 23:43:30 +0300

    clear

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -8,26 +8,26 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static final int COUNT_ROUTES = 1000;
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+
     public static void main(String[] args) throws InterruptedException {
         Runnable runnable = () -> {
             String route = generateRoute("RLRFR", 100);
             int countR = 0;
             for (char r : route.toCharArray()) {
-                if(r == 'R') {
+                if (r == 'R') {
                     countR++;
                 }
             }
             synchronized (sizeToFreq) {
-               if (sizeToFreq.containsKey(countR)) {
-                   sizeToFreq.put(countR, sizeToFreq.get(countR) + 1);
-               } else {
-                   sizeToFreq.put(countR, 1);
-               }
+                if (sizeToFreq.containsKey(countR)) {
+                    sizeToFreq.put(countR, sizeToFreq.get(countR) + 1);
+                } else {
+                    sizeToFreq.put(countR, 1);
+                }
             }
         };
 
         final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-//        List<Future> futures = new ArrayList<>();
         for (int i = 0; i < COUNT_ROUTES; i++) {
             pool.submit(runnable);
         }
@@ -37,15 +37,15 @@ public class Main {
         int maxRepeatKey = 0;
         int maxRepeatValue = 0;
 
-        for(Map.Entry<Integer, Integer> entry: sizeToFreq.entrySet()) {
-            if(entry.getValue() > maxRepeatValue) {
+        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+            if (entry.getValue() > maxRepeatValue) {
                 maxRepeatValue = entry.getValue();
                 maxRepeatKey = entry.getKey();
             }
         }
         System.out.println("Самое частое количество повторений " + maxRepeatKey + " (встретилось " + maxRepeatValue + " раз)");
         System.out.println("Другие размеры:");
-        for(Map.Entry<Integer, Integer> entry: sizeToFreq.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
             if (entry.getKey() != maxRepeatKey) {
                 System.out.println(entry.getKey() + " повторений встретилось " + entry.getValue() + " раз");
             }
